4e556dc178cb1ffa24318a4ae3f1540f221c1ea1
Arjun-93 <arjun20178@iiitd.ac.in>
2023-03-26 21:21:47 +0530

    webpageupdated

--- Backend/myapp.py
+++ Backend/myapp.py
@@ -3,9 +3,8 @@ from mysql.connector import errorcode
 from flask import Flask, request
 
 try:
-    cnx = mysql.connector.connect(user='yourusername', password='yourpassword',
-                              host='yourhost',
-                              database='yourdatabase')
+    cnx = mysql.connector.connect(user='root', password='Arjun$123',port=3306, host='127.0.0.1',database='retailstore',
+    auth_plugin='mysql_native_password')
 except mysql.connector.Error as err:
     if err.errno == errorcode.ER_ACCESS_DENIED_ERROR:
         print("Something is wrong with your user name or password")
@@ -55,6 +54,26 @@ def update_form():
     
     return 'Form updated successfully'
 
+@app.route('/customer_sign-form', methods=['POST'])
+def customer_sign():
+    username = request.form['username']
+    password = request.form['password']
+
+    # check if data exist in the database , if exist make sign in else signup
+    # if( password):
+
+
+from flask import Flask
+from flask_mysqldb import MySQL
+
+app = Flask(__name__)
+
+app.config['MYSQL_HOST'] = 'localhost'  # replace with your MySQL host
+app.config['MYSQL_USER'] = 'root'  # replace with your MySQL username
+app.config['MYSQL_PASSWORD'] = 'Arjun$123'  # replace with your MySQL password
+app.config['MYSQL_DB'] = 'retailstore'  # replace with your MySQL database name
+
+mysql = MySQL(app)
 
 if __name__ == '__main__':
     app.run()
